4047ccb752c2e13e6e413c1622d3b6afc5deb2ce
Johnny Meneses <johnny.meneses@outlook.com>
2022-03-27 12:20:50 -0300

    Ex02

--- app/src/main/java/com/kotlin_poo/Ex02.kt
+++ app/src/main/java/com/kotlin_poo/Ex02.kt
@@ -1,10 +1,15 @@
-
+package com.kotlin_poo
 /*
 	A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
     O cálculo da área neste caso é bem simples, basta multiplicar lado x comprimento para retângulos
 * */
 
-fun main() = println(ex02(5, 4))
+fun main() = println(Ex02(5, 4))
+//fun main() = Ex02a(5, 4)
 
 
-fun ex02(a: Int, b: Int): String = "A area é de ${a * b}"
+fun Ex02(a: Int, b: Int): String = "A área do terreno é ${+a * b}"
+
+fun Ex02a(a: Int, b: Int) {
+    println("A área do terreno é ${+a * b}")
+}
