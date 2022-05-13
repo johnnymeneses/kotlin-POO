59d03a85ebbbf7bb44c3cebc33d0bb4e1380dc7d
Johnny Meneses <bilbo_vader@outlook.com>
2022-05-12 22:14:23 -0300

    Revisao

--- app/src/main/java/main.kt
+++ app/src/main/java/main.kt
@@ -13,7 +13,7 @@ fun main() {
         4 -> println(Ex04(10, 5))                   // Divisao de dois numeros
         5 -> Ex05(10, 11)                       //Padaria
         6 -> println(Ex06("Johnny", 37))      //Idade em anos
-        7 -> println(Ex07(5.69f,5.69f)) //Preço Gasolina
+        7 -> println(Ex07(5.69f,5.69f))     //Preço Gasolina
         8 -> Ex08()
         9 -> Ex09()
         10 -> Ex10()
