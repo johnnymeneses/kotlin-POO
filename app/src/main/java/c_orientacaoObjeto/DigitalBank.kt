ad22018f52efc03bfdf698801e8e74709fab009f
Johnny Meneses <bilbo_vader@outlook.com>
2022-06-10 20:01:45 -0300

    Membros Classe 3

--- app/src/main/java/c_orientacaoObjeto/DigitalBank.kt
+++ app/src/main/java/c_orientacaoObjeto/DigitalBank.kt
@@ -1,23 +1,40 @@
 package c_orientacaoObjeto
 
 
-class Pessoa{
+class Pessoa {
+
+    //Propriedades da classe
     var nome: String = "Johnny"
-    var cpf: String = "123.456.789-00"
+
+    var cpf: String = "123.456.789-00" //Campo Privado
+        private set
+
+//    inner class Endereco{                           //classe interna
+//        var rua: String = "Rua show"
+//    }
+
+
 }
 
+class Bank {}
+
+class Account {}
 
 
 fun main() {
-    println("DigitalBank")
+    println("DigitalBank App")
 
     val cliente = Pessoa()
 
+    cliente.nome = "Novo Nome"
+
+
+
+
+    println(cliente) //Referencia do Objeto
     println(cliente.nome)
     println(cliente.cpf)
+//    println(cliente.Endereco().rua)
 
 
 }
-
-//Classe publica e Privada
-//
