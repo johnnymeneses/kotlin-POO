b6bdf2b8005aa2cb9949247a49344b674c696329
Johnny Meneses <bilbo_vader@outlook.com>
2022-06-10 19:44:44 -0300

    Classe Pessoa

--- app/src/main/java/c_orientacaoObjeto/DigitalBank.kt
+++ app/src/main/java/c_orientacaoObjeto/DigitalBank.kt
@@ -1,12 +1,22 @@
 package c_orientacaoObjeto
 
 
-class Pessoa{}
+class Pessoa{
+    var nome: String = "Johnny"
+    var cpf: String = "123.456.789-00"
+}
 
 
 
 fun main() {
     println("DigitalBank")
+
+    val cliente = Pessoa()
+
+    println(cliente.nome)
+    println(cliente.cpf)
+
+
 }
 
 //Classe publica e Privada
